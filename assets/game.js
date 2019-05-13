95563b01222bfc73e588f77da1f769be8b535ae4
GumpFlash <gump.flash@gmail.com>
2019-05-13 20:18:11 -0300

    New2

--- assets/game.js
+++ assets/game.js
@@ -25,6 +25,8 @@ function init(){
     setInterval(() => {
         Xp.Add(1);
     }, 10000);
+
+    Control.init();
 }
 
 var Gold = {
@@ -122,15 +124,70 @@ var Tab = {
         if(t == 0){
             f("#tab1").style.display = "block";
             f("#tab2").style.display = "none";
+            f("#btn_tab0").className = "";
             f("#btn_tab1").className = "active";
             f("#btn_tab2").className = "";
             f("#tab1").style.width = "98%";
-        }else{
+        }else if(t == 1){
             f("#tab1").style.display = "none";
             f("#tab2").style.display = "block";
+            f("#btn_tab0").className = "";
             f("#btn_tab1").className = "";
             f("#btn_tab2").className = "active";
             f("#tab2").style.width = "98%";
+        }
+    }
+}
+
+
+var x = 0;
+var y = 0;
+var maxX = 500;
+var maxY = 500;
+var Control = {
+    walk : function(dirx,diry){
+        dirx *= 50;
+        diry *= 50;
+
+        x += dirx;
+        y += diry;
+        if(x > maxX || y > maxY){
+            x -= dirx;
+            y -= diry;
+        }else{
+            window.scrollTo(x,y);
+        }   
+    },
+    init : function(){
+        window.scrollTo(x,y);
+
+        window.addEventListener('keydown',function(e){
+            if(e.key == "ArrowDown")
+                Control.walk(0,1);
+            if(e.key == "ArrowUp")
+                Control.walk(0,-1);
+            if(e.key == "ArrowLeft")
+                Control.walk(-1,0);
+            if(e.key == "ArrowRight")
+                Control.walk(1,0);
+        });
+    }
+}
+
+var Inventory = {
+    slots : slots = [],
+    itens : itens = [
+        "c|=><br>Espada Fajuta"
+    ],
+    add : function (id){
+        if(Inventory.slots.length < 9){
+            Inventory.slots.push(Inventory.itens[id]);
+            Inventory.show();
+        }
+    },
+    show : function (){
+        for(var i = 0; i < Inventory.slots.length; i++){
+            f("#slot" + i).innerHTML = Inventory.slots[i];
         }
     }
 }
